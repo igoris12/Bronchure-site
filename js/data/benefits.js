2de65b7c63f237b1dcc47026728544293d001170
Igoris Ivanovas <igoris.x@inbox.lt>
2021-06-23 21:24:08 +0300

    end benefit style

--- js/data/benefits.js
+++ js/data/benefits.js
@@ -2,19 +2,19 @@ const Data = {
     imgPath: "./img/benefints/",
     list: [
         {
-            img: "",
+            img: "Safe.png",
             title: "Safe",
             description: "The robot provides a better work environment by removing dangerous, repeatable and dirty work for humans."
 
         },
         {
-            img: "",
+            img: "Easy-to-use.png",
             title: "Easy-to-use & Fast to set-up",
             description: "Intuitive software interface allows quickly to set up any bending task without special training."
 
         },
         {
-            img: "",
+            img: "Mobile.png",
             title: "Mobile",
             description: "Global Technologies cell can easily be moved away in need of manual use of the machine or to another press brake due to the unique interface."
 
@@ -26,13 +26,13 @@ const Data = {
 
         },
         {
-            img: "",
+            img: "Mobile.png",
             title: "Speed",
             description: "Robots don’t get distracted or need to take breaks. They don’t request vacation time or ask to leave an hour early."
 
         },
         {
-            img: "",
+            img: "Perfection.png",
             title: "Perfection",
             description: "Robots will always deliver quality. Since they’re programmed for precise, repetitive motion, they’re less likely to make mistakes. In some ways, robots are simultaneously an employee and a quality control system."
 
