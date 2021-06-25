63f1600dead505f3d68d770981e2661e7f7cbfdd
Igoris Ivanovas <igoris.x@inbox.lt>
2021-06-25 22:01:26 +0300

    js render

--- js/home.js
+++ js/home.js
@@ -1,4 +1,8 @@
 import { Benefints } from "./component/benefits.js";
+import { Footer } from "./component/footer.js";
 import { data } from "./data/benefits.js"
+import { footerData } from "./data/footerData.js";
 
 new Benefints(".benefits> .row", data);
+
+new Footer(".footer .gallery", footerData)
