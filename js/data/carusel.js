ae7b76a966839455dded5360810e322ffb2eb547
Igoris Ivanovas <igoris.x@inbox.lt>
2021-07-03 22:03:05 +0300

    corusel start copy

--- js/data/carusel.js
+++ js/data/carusel.js
@@ -1,59 +1,73 @@
 const CaruselData = {
-    imgPath: 'a',
-    list: [
-        {
-            title: 'content',
-            src: 'aaa'
-        },
-        {
-            title: 'content',
-            src: 'aaa'
-        },
-        {
-            title: 'content',
-            src: 'aaa'
-        },
-        {
-            title: 'content',
-            src: 'aaa'
-        },
-        {
-            title: 'content',
-            src: 'aaa'
-        },
-        {
-            title: 'content',
-            src: 'aaa'
-        },
-        {
-            title: 'content',
-            src: 'aaa'
-        },
-        {
-            title: 'content',
-            src: 'aaa'
-        }
-    ],
+    imgPath: "/img/course-benefit-img/",
     itemsInScrean: [
         {
-            minWith: 1100,
-            itemsInScrean: 5
+            minWidth: 0,
+            itemsNumber: 1
         },
         {
-            minWith: 800,
-            itemsInScrean: 3
+            minWidth: 750,
+            itemsNumber: 2
         },
         {
-            minWith: 500,
-            itemsInScrean: 2
+            minWidth: 990,
+            itemsNumber: 3
         },
         {
-            minWith: 0,
-            itemsInScrean: 1
+            minWidth: 1100,
+            itemsNumber: 4
+        }],
+
+    itemsInScrean: [
+        {
+            minWidth: 0,
+            itemsNumber: 1
+        },
+        {
+            minWidth: 880,
+            itemsNumber: 2
+        },
+        {
+            minWidth: 1100,
+            itemsNumber: 3
+        }],
+    list: [
+        {
+            src: 'mentoring.png',
+
+            title: '1-1 Mentoring',
+
+
+        },
+        {
+            src: '100online.png',
+
+            title: '100% online',
+
+
+        },
+        {
+            src: 'flexibleDeadline.png',
+
+            title: 'Flexible Deadline',
+
+
+        }, {
+            src: 'mentoring.png',
+
+            title: '1-1 Mentoring',
+
+        },
+        {
+            src: 'projectReviews.png',
+
+            title: 'Project Reviews',
+
+
         },
 
     ]
-};
+}
 
 
 
