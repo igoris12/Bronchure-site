9dee83b3ee952bedc4b1eb8697e34502cdf52a88
Igoris Ivanovas <igoris.x@inbox.lt>
2021-07-07 21:29:10 +0300

    data

--- js/data/carusel.js
+++ js/data/carusel.js
@@ -18,30 +18,37 @@ const CaruselData = {
         {
             src: 'server.png',
             title: 'Stable servers',
+            discrioption: 'Our server are most stable and using latest process for stability.'
         },
         {
             src: 'support.png',
-            title: 'Technical support ',
+            title: 'Technical support',
+            discrioption: 'Our support teams are ready full day give help.'
         },
         {
             src: 'manitoring.png',
             title: 'Full time monitoring',
+            discrioption: 'We are monitoring full time all work process that is necessary.'
         },
         {
             src: 'mashine.png',
             title: 'Latest technical products',
+            discrioption: 'Product that we are offering is newest in the market and leading in the technical.'
         },
         {
             src: 'offer.png',
             title: 'Best offer in market',
+            discrioption: 'Our offer is best in market we offer great quality and price.'
         },
         {
             src: 'progress.png',
             title: 'Ways to boost your progress',
+            discrioption: 'With us you will be getting best tools to update you progress.'
         },
         {
             src: 'automation.png',
             title: 'Automatizate work process',
+            discrioption: 'The best thing is that all process are maximum automated.'
         },
 
 
