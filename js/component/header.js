9aa5d7f7ef6bda20020d0b44d295322c41ff7f03
Igoris Ivanovas <igoris.x@inbox.lt>
2021-07-08 22:03:48 +0300

    scroll evern start

---
+++ js/component/header.js
@@ -0,0 +1,15 @@
+
+
+window.addEventListener('scroll', function () {
+
+    let scrollY = Math.round(window.scrollY)
+
+    console.log(scrollY);
+})
+
+// function scrollHeader() {
+
+//     if (window.scrollY = 300) {
+//         console.log('sss');
+//     }
+// }
